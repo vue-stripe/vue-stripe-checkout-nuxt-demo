58f6d7617680275dca1783e079fa8b48beb8f42d
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-09-23 22:00:02 +0800

    refactor: update vuetify theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors';
-
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -56,16 +54,24 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
+        light: {
+          primary: '#635BFF',
+          accent: '#E49F48',
+          secondary: '#3297D3',
+          info: '#45B2E8',
+          warning: '#E37C4C',
+          error: '#C23D4B',
+          success: '#24B47E',
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          primary: '#635BFF',
+          accent: '#E49F48',
+          secondary: '#3297D3',
+          info: '#45B2E8',
+          warning: '#E37C4C',
+          error: '#C23D4B',
+          success: '#24B47E',
         },
       },
     },
