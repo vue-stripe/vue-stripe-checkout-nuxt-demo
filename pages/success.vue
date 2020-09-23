db459cf32beac14129805fa74cdeda029702ed44
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-09-23 21:51:24 +0800

    feat: create success/cancel pages

---
+++ pages/success.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-container(fill-height)
+    v-row(justify="center" align="center")
+      v-col(cols="12" md="10")
+        v-card(outlined)
+          v-card-text.text-center
+            h1 Success Page
+          v-card-actions
+            v-spacer
+            v-btn(
+              depressed
+              large
+              color="primary"
+              to="/"
+            ).text-none Back to home
+</template>
