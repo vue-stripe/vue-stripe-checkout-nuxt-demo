6de6eba3fe202b8fb1a376f6dc52e3ae79a19db9
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-06-24 17:21:02 +0800

    refactor: update demo page
    - update layout
    - re-implement checkout
    - add elements > card demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,119 @@
 <template lang="pug">
-  v-container(fill-height)
-    v-row(justify="center" align="center")
+  v-container
+    //- v-row(justify="center")
+      v-col(cols="12" md="12").text-center
+    v-row(justify="center").mt-16
+      v-col(cols="12" md="12").text-center
+        img(width="150" src="../assets/vue-stripe-logo-variant-1-small.png")
       v-col(cols="12" md="6")
         v-card(outlined)
+          v-toolbar(flat color="#f7f7f7")
+            h2 Stripe Checkout - One-time Payment
+            v-spacer
+            a(href="https://vuestripe.com/stripe-checkout/one-time-payment"  target="_blank") See Docs
           v-card-text.text-center
-            img(width="150" src="../assets/vue-stripe-logo-variant-1-small.png")
-            br
+            img(width="200" src="../assets/common-sense.jpeg")
+            h2 Buy common sense for $10.00
             stripe-checkout(
               ref="checkoutRef"
+              mode="payment"
               :pk="pk"
-              :items="items"
-              :successUrl="successUrl"
-              :cancelUrl="cancelUrl"
+              :line-items="lineItems"
+              :success-url="successURL"
+              :cancel-url="cancelURL"
+              @loading="v => loading = v"
             )
-              template(slot="checkout-button")
-                v-btn(
-                  depressed
-                  large
-                  color="primary"
-                  :loading="loading"
-                  :disabled="loading"
-                  @click="checkout"
-                ).text-none Nuxt Checkout Demo
+            br
+            v-alert(
+              dark
+              v-if="redirectState"
+              :color="redirectState"
+              dismissible
+            ) Payment {{redirectState}}
+          v-card-actions
+            v-spacer
+            v-btn(
+              depressed
+              large
+              color="primary"
+              :loading="loading"
+              :disabled="loading"
+              @click="checkout"
+            ).text-none Pay $10
+      v-col(cols="12" md="6")
+        v-card(outlined)
+          v-toolbar(flat color="#f7f7f7")
+            h2 Stripe Elements - Card
+            v-spacer
+            a(href="https://vuestripe.com/stripe-elements/card"  target="_blank") See Docs
+          v-card-text.pt-10
+            stripe-element-card(
+              ref="cardRef"
+              :pk="pk"
+              hide-postal-code
+              @token="tokenCreated"
+              @loading="v => loading = v"
+            )
+            pre {{token}}
+          v-card-actions
+            v-spacer
+            v-btn(
+              depressed
+              large
+              color="primary"
+              :loading="loading"
+              :disabled="loading"
+              @click="$refs.cardRef.submit()"
+            ).text-none Generate token
 </template>
 
 <script>
 import headMeta from '~/utils/head-meta';
+
 export default {
   data () {
     this.pk = process.env.STRIPE_PK;
     return {
       loading: false,
-      items: [
+      lineItems: [
         {
-          sku: 'sku_FdQKocNoVzznpJ',
+          price: 'sku_FdQKocNoVzznpJ', // The id of the one-time price you created in your Stripe dashboard
           quantity: 1,
         },
       ],
+      token: null,
+      successURL: process.client && `${window.location.origin}${window.location.pathname}?state=success`,
+      cancelURL: process.client && `${window.location.origin}${window.location.pathname}?state=error`,
+      // Misc
+      redirectState: '',
     };
   },
   computed: {
-    successUrl () {
-      return process.browser ? `${window.location.origin}/success` : '';
+    queryState () {
+      return process.client && (new URLSearchParams(window.location.search).get('state') || '');
     },
-    cancelUrl () {
-      return process.browser ? `${window.location.origin}/cancel` : '';
-    },
+  },
+  mounted () {
+    if (this.queryState === 'success') {
+      this.redirectState = 'success';
+    }
+    if (this.queryState === 'error') {
+      this.redirectState = 'error';
+    }
   },
   methods: {
     checkout () {
       this.loading = true;
       this.$refs.checkoutRef.redirectToCheckout();
     },
+    tokenCreated (token) {
+      this.token = token;
+    },
   },
   head () {
     return headMeta({
-      title: 'Vue Stripe Checkout + Nuxt.js Demo',
-      description: 'A demo on how to implement vue-stripe-checkout in Nuxt.js',
+      title: 'Vue Stripe + Nuxt.js Demo',
+      description: 'A demo on how to implement Vue Stripe in Nuxt.js',
       socialBanner: require('../assets/vue-stripe-logo-variant-1-small.png'),
     });
   },
